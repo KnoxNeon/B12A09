6a7f42be8efebd0024955046710d4b9543abaa96
KnoxNeon <iftey10@gmail.com>
2025-11-25 22:50:16 +0600

    add popular game section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,14 +7,14 @@ import Slider from '../Components/Slider'
 
 const Home = () => {
      const {games, loading, error} = useGames()
-     const featuredGames = games.slice(0,8)
+     const featuredGames = games.filter(game => game.ratings > 4.8)
   return (
 
     <div>
       <div >
         <Slider/>
-        <h1 className='pt-4 lg:mt-20 mb-2 text-3xl lg:text-5xl font-bold text-center'>Trending Apps</h1>
-      <p className='text-[#627382] text-md lg:text-xl text-center pb-10'>Explore All Trending Apps on the Market developed by us</p>
+        <h1 className='pt-4 lg:mt-20 mb-2 text-3xl lg:text-5xl font-bold text-center'>Popular Games</h1>
+      <p className='text-[#627382] text-md lg:text-xl text-center pb-10'>Explore All Trending Games Played By Gamers</p>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6 lg:my-10 m-6 lg:mx-20'>
         {
           featuredGames.map(game =>(
